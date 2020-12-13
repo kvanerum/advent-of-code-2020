9cf5819551a907d27a4dd36e2880fded60d3a316
kvanerum <krisvanerum@gmail.com>
2020-12-13 13:06:58 +0100

    Day 6

---
+++ src/day6/Main.java
@@ -0,0 +1,56 @@
+package day6;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        List<String> groups = readInput();
+
+        long count = groups.stream().map(group ->
+                group.trim().replace("\n", "").chars().mapToObj(c -> (char) c).collect(Collectors.toSet()).size()
+        ).reduce(0, Integer::sum);
+        System.out.println(count);
+
+        long count2 = groups.stream().map(group -> {
+            Map<Character, Integer> buckets = new HashMap<>();
+
+            List<String> lines = group.lines().filter(line -> !line.isEmpty()).map(String::trim).collect(Collectors.toList());
+            lines.forEach(line -> {
+                for (Character c : line.toCharArray()) {
+                    buckets.put(c, buckets.getOrDefault(c, 0) + 1);
+                }
+            });
+
+            return buckets.entrySet().stream().filter(entry -> entry.getValue() == lines.size()).count();
+        }).reduce(0L, Long::sum);
+        System.out.println(count2);
+    }
+
+    private static List<String> readInput() throws IOException {
+        List<String> lines = Files.lines(Paths.get("src/day6/input.txt")).collect(Collectors.toList());
+
+        List<String> groups = new ArrayList<>();
+        String current = "";
+
+        for (String line : lines) {
+            if (line.length() == 0) {
+                if (!current.isEmpty()) {
+                    groups.add(current);
+                    current = "";
+                }
+            } else {
+                current += "\n" + line;
+            }
+        }
+
+        if (!current.isEmpty()) {
+            groups.add(current);
+        }
+
+        return groups;
+    }
+}
